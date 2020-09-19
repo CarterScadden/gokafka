a0736b355daea8d67c2e0868e2a3def5cf34b86d
Tanner Scadden <tanner@scaddenfamily.com>
2020-09-19 13:17:52 -0600

    got socket server working right

--- main.go
+++ main.go
@@ -1,18 +1,66 @@
+// package main
+
+// import (
+// 	"gokafka/server"
+// 	"net/http"
+// )
+
+// func main() {
+// 	//read user input
+// 	// if input == "c"
+// 	// run client stuff
+// 	// else run server stuff
+// 	// and run two terminals for both
+
+
+// 	server := server.Server{}
+
+// 	http.HandleFunc("/socket", server.Run)
+
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		http.ServeFile(w, r, "websockets.html")
+// 	})
+
+// 	http.ListenAndServe(":8080", nil)
+// }
+
+// Copyright 2013 The Gorilla WebSocket Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
-	"gokafka/server"
+	"flag"
+	"log"
 	"net/http"
 )
 
+var addr = flag.String("addr", ":8080", "http service address")
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	http.ServeFile(w, r, "home.html")
+}
+
 func main() {
-	server := server.Server{}
-
-	http.HandleFunc("/socket", server.Run)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "websockets.html")
+	flag.Parse()
+	hub := newHub()
+	go hub.run()
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(hub, w, r)
 	})
-
-	http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(*addr, nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
